d2ba9c253a72483eb67961dc57dcb0a1d690d37f
Bert Riffelmacher <sotsoguk@gmail.com>
2020-12-02 14:11:38 +0100

    +Day01/02 in Python3

---
+++ python/day01/day01.py
@@ -0,0 +1,69 @@
+import os
+import numpy as np
+from itertools import combinations
+
+import time
+
+def main():
+    # input
+    print(os.getcwd())
+    day = "01"
+    inputFile = f'../inputs/input{day}.txt'
+    print(inputFile)
+
+    with open(inputFile) as f:
+        lines = f.read().splitlines()
+
+    nlist = [int(i) for i in lines]
+    nset = set(nlist)
+    target = 2020
+    
+    f
+    # print(len(lines))
+    start_time = time.time()
+    # for i in range(10000000):
+    #     t = np.random.choice(nlist,3,replace = False)
+    #     if sum(t) == target:
+    #         print(i,t,t[0]*t[1]*t[2])
+    #         break
+    # part1
+    part1 = 0
+    for i in nlist:
+        if (target - i) in nset:
+            part1 = i * (target-i)
+
+
+    # part2
+    part2 = 0
+    for i in range(len(nlist)):
+        for j in range(i,len(nlist)):
+            missing = target - (nlist[i]+nlist[j])
+            if missing in nset:
+                part2 = nlist[i] * nlist[j] * missing
+     
+    
+    duration = int((time.time() - start_time) * 1000000)
+
+    print(f"AoC 2020\nDay {day}:\n\nPart 1:\t{part1}\nPart 2:\t{part2}\nDuration:\t{duration} ns")
+
+    # # alternative using itertools
+    # # BUT: much slower (2 ms vs 50 ms)
+
+
+    # start_time2 = time.time()
+    
+    # for c in combinations(nlist,2):
+    #     if sum(c) == target:
+    #         part1 = c[0] * c[1]
+    #         break
+
+    # for c in combinations(nlist,3):
+    #     if sum(c) == target:
+    #         part1 = c[0] * c[1] *c[2]
+    #         break
+
+    # duration = int((time.time() - start_time2) * 1000000)
+    
+    # print(f"AoC 2020\nDay {day}:\n\nPart 1:\t{part1}\nPart 2:\t{part2}\nDuration:\t{duration} ns")
+if __name__ == "__main__":
+    main()
---
+++ python/day02/day02.py
@@ -0,0 +1,40 @@
+import os
+import time
+
+def main():
+    # input
+    print(os.getcwd())
+    day = "02"
+    part1, part2 = 0, 0
+    starLine = "*" * 19
+    resultString = 
+    inputFile = f'../inputs/input{day}.txt'
+
+    with open(inputFile) as f:
+        lines = f.read().splitlines()
+    
+    start_time = time.time()
+
+    for l in lines[:]:
+        # extract data
+
+        toks = l.split()
+        minOcc, maxOcc = [int(i) for i in toks[0].split('-')]
+        char, password = toks[1][0], toks[2]
+        occ = sum([1 for c in password if c == char])
+
+        # part 1
+        if minOcc <= occ <= maxOcc:
+            part1 += 1
+
+        # part 2
+        if bool(password[minOcc-1] == char) ^ bool(password[maxOcc-1] == char):
+            part2 += 1
+
+    duration = int((time.time() - start_time) * 1000000)
+    print(
+        f"\n{starLine}\n AoC 2020 - Day {day}\n{starLine}\n\nPart 1:\t\t{part1}\nPart 2:\t\t{part2}\nDuration:\t{duration} ns")
+
+
+if __name__ == "__main__":
+    main()
