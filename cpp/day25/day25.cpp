b809ae251a2f240e376da30e51ac98ff5f260633
Bert Riffelmacher <sotsoguk@gmail.com>
2020-12-26 02:17:54 +0100

    measure time day25 c++

--- cpp/day25/day25.cpp
+++ cpp/day25/day25.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <chrono>
 // #include "reader.hpp"
 
 int main()
 {
     uint64_t i1(2069194), i2(16426071);
     uint64_t modv(20201227), sn(7), l1(1), v(1), v2(1);
-
+    auto t1 = std::chrono::high_resolution_clock::now();
     while (true)
     {
         v *= sn;
@@ -16,7 +17,8 @@ int main()
         {
             break;
         }
-        l1++;
     }
-    std::cout << "Encryption Key: " << v2 << std::endl;
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds> (t2-t1).count();
+    std::cout << "Encryption Key: " << v2 << " (" << duration << ")" <<std::endl;
 }
